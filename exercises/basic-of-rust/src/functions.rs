f302541467a47b166c740ad120f847f2bd027fce
TuananhSR <105163640+TuananhSR@users.noreply.github.com>
2023-07-05 17:59:01 +0700

    push

--- exercises/basic-of-rust/src/functions.rs
+++ exercises/basic-of-rust/src/functions.rs
@@ -1,7 +1,7 @@
 // Exercise 1
 // Fix all errors 
-fn sum(x, y: i32) {
-    x + y;
+fn sum(x: i32 , y: i32) -> i32{
+    return x + y;
 }
 
 //Exercise 2
@@ -11,7 +11,11 @@ fn sum(x, y: i32) {
 pub fn sum_one_to_n(n: u32) -> u32 {
     // your code for summing all digits from 1 to `n` (inclusive) should go
     // here (you can remove the sample return of `0`)
-    0
+    let mut sum = 0;
+    for i in 1..=n {
+        sum +=i;
+    }
+    return sum;
 }
 
 // Exercise 3
@@ -19,13 +23,27 @@ pub fn sum_one_to_n(n: u32) -> u32 {
 // Problem: Calculate the average of a list of numbers
 // Output: Average Number 
 fn calculate_average(numbers: &[f64]) -> f64 {
-    todo!()
+    if numbers.len() == 0 {
+        return 0.0
+    }
+    let mut sum: f64 = 0.0;
+    for &num in numbers {
+        sum += num;
+    }
+    let average: f64 = sum / (numbers.len() as f64);
+    average
 }
 
 // Exercise 4
 // Calculate the sum of all even numbers in a list
 fn sum_even_numbers(numbers: &[i32]) -> i32 {
-    todo!()
+    let mut evensum: i32 = 0;
+    for &num in numbers {
+        if num % 2 ==0 {
+            evensum += num;
+        }
+    }
+    return evensum;
 }
 
 
--- exercises/basic-of-rust/src/strings.rs
+++ exercises/basic-of-rust/src/strings.rs
@@ -1,24 +1,27 @@
 // Exercise 1
 #[allow(dead_code)]
 fn exercise1(color: &str) -> String {
-    todo!()
+    match color {
+        "white" => String::from("white"),
+        _ => String::from("unknown")
+    }
 }
 
 // Exercise 2
 // Fix all errors without adding newline
 fn exercise2() -> String {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
     s.push(',');
-    s.push(" world");
-    s += "!".to_string();
+    s.push_str(" world");
+    s.push('!');
     s
 }
 // Exercise 3
 // Fix errors without removing any line
 fn exercise3() -> String {
     let s1 = String::from("hello,");
-    let s2 = String::from("world!");
-    let s3 = s1 + s2;
+    let s2 = String::from(" world!");
+    let s3 = format!("{}{}", s1, s2);
     s3
 }
 
@@ -26,20 +29,35 @@ fn exercise3() -> String {
 // Reverse a string
 
 fn reverse_string(input: &str) -> String {
-    todo!()
+    let reversed: String = input.chars().rev().collect();
+    reversed
 }
 
 
 // Exercise 5
 // Check if a string is a palindrome
 fn is_palindrome(word: &str) -> bool {
-    todo!()
+    let chars: Vec<char> = word.chars().collect();
+    let len = chars.len();
+    for i in 0..len / 2 {
+        let j = len - 1 - i;
+        if chars[i].to_lowercase().to_string() != chars[j].to_lowercase().to_string() {
+            return false;
+        }
+    }
+    true
 }
 
 // Exercise 6
 // Count the occurrences of a character in a string
 fn count_char_occurrences(string: &str, ch: char) -> usize {
-    todo!()
+    let mut count = 0;
+    for c in string.chars() {
+        if c == ch {
+            count += 1;
+        }
+    }
+    return count;
 }
 
 #[cfg(test)]
@@ -92,7 +110,7 @@ mod tests {
     #[test]
     fn test_count_char_occurrences() {
         assert_eq!(count_char_occurrences("Hello", 'l'), 2);
-        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 1);
+        assert_eq!(count_char_occurrences("Rust is fun", 'u'), 2);
         assert_eq!(count_char_occurrences("Mississippi", 's'), 4);
     }
 
